5466d91b49f79b97a62c5cf308197b9c8874fbc2
Felipe Ramalho <frs.ramalho.santos@gmail.com>
2025-08-09 16:04:59 -0300

    ajuste section experiencias profissionais e header

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const elements = document.querySelectorAll('.fade-in, .texto-rolagem'); // Adicionei .texto-rolagem aqui
+    const elements = document.querySelectorAll('.fade-in');
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('aparece');
-                observer.unobserve(entry.target); // Deixa de observar após o efeito
+                observer.unobserve(entry.target);
             }
         });
     }, { threshold: 0.5 });
 
-    elements.forEach(element => observer.observe(element)); // Observa os elementos
+    elements.forEach(element => observer.observe(element));
 });
